a6d3e20b389607719c3e0927e973a1f80ddb5737
unknown <eldergsilva@gmail.com>
2024-04-12 13:01:27 -0300

    implementado pesquisar videos

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,29 +1,30 @@
-async function listaVideos(){
-    const conexao = await fetch("http://localhost:3000/videos")
-    const conexaoConvertida  = await conexao.json();
-    
+async function listaVideos() {
+    const conexao = await fetch("http://localhost:3000/videos");
+    const conexaoConvertida = await conexao.json();
+
     return conexaoConvertida;
 }
 
-async function criaVideo (titulo,descricao,url,imagem) {
-    const conexao = await fetch("http://localhost:3000/videos",{
-        method:"POST",
-        headers:{
-            "Content-type":"application/json"
+async function criaVideo(titulo, descricao, url, imagem) {
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
         },
-        body:JSON.stringify({
-          titulo:titulo,
-          descricao:`${descricao} mil visualizações`,
-          url:url,
-          imgagem:imagem  
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
         })
+    });
 
-    }) 
-    const conexaoConvertida =await conexao.json();
+    const conexaoConvertida = conexao.json();
+
     return conexaoConvertida;
 }
 
-export const conectaApi ={
+export const conectaApi = {
     listaVideos,
     criaVideo
 }
